953d7e4b1734512ec162a6720dfbabb365ca91fb
Pere Montpeo <pere.montpeo@decathlon.com>
2023-03-15 15:36:12 +0100

    vtmn added

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,37 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import '@vtmn/css/dist/index.css'
+import Head from 'next/head'
+import { useState } from 'react'
+import styles from './index.module.css'
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [animalInput, setAnimalInput] = useState('')
+  const [result, setResult] = useState()
 
   async function onSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+        body: JSON.stringify({ animal: animalInput })
+      })
 
-      const data = await response.json();
+      const data = await response.json()
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        )
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setResult(data.result)
+      setAnimalInput('')
+    } catch (error) {
       // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+      console.error(error)
+      alert(error.message)
     }
   }
 
@@ -40,19 +44,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3 className="vtmn-typo_title-3">Name my pet</h3>
         <form onSubmit={onSubmit}>
           <input
+            className="vtmn-text-input"
             type="text"
             name="animal"
             placeholder="Enter an animal"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input
+            type="submit"
+            className="vtmn-btn vtmn-btn_size--stretched"
+            value="Generate names"
+          />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
     </div>
-  );
+  )
 }
