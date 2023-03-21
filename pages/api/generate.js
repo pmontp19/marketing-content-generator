296b115d3108ff73c0df7501017907733a383a14
Pere Montpeo <pere.montpeo@decathlon.com>
2023-03-21 20:22:38 +0100

    communication types

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,13 @@
 import { Configuration, OpenAIApi } from 'openai'
 
+// constants
+const EVENT = 'event'
+const MAINTENANCE = 'maintenance'
+const NEWPRODUCT = 'new-product'
+const SUSTAINABILITY = 'sustainability-event'
+const COMMON_PROMPT = `Eres MarketingExpertoGPT y estás especializado en desarrollar e implementar campañas de marketing. Ofreciendo guiado en contenido, objetivo, mensaje, y tácticas promocionales, asistes a los usuarios en promocionar sus productos o servicios y alcanzar su audiencia. Elabora un mensaje para un cliente, informal, en primera persona del plural. Responde en dos partes: primero el asunto terminado con un emoji, y después el cuerpo del mensaje. Sin el saludo ni la despedida, ni la firma. Escribe la fecha en formato texto. Devuelve la respuesta en formato json, una clave para el asunto y otra para el mensaje, como el siguiente esquema {"asunto": "asunto", "mensaje":"mensaje"}.`
+const CATALAN_PROMPT = 'Escribe en catalán.'
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
@@ -16,11 +24,11 @@ export default async function (req, res) {
     return
   }
 
-  const animal = req.body.animal || ''
-  if (animal.trim().length === 0) {
+  const subject = req.body.subject || ''
+  if (subject.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: 'Please enter a valid animal'
+        message: 'Please enter a valid input'
       }
     })
     return
@@ -29,7 +37,17 @@ export default async function (req, res) {
   try {
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: generateContent(animal)}],
+      messages: [
+        {
+          role: 'user',
+          content: generateSwitcher(
+            req.body.date,
+            req.body.type,
+            req.body.subject,
+            req.body.language
+          )
+        }
+      ],
       temperature: 0.6
     })
     res.status(200).json({ result: completion.data.choices[0].message.content })
@@ -47,6 +65,44 @@ export default async function (req, res) {
       })
     }
   }
+}
+
+function generateSwitcher(date, type, subject, language) {
+  switch (type) {
+    case EVENT:
+      return generateEvent(date, subject, language)
+    case MAINTENANCE:
+      return generateMaintenance(subject, language)
+    case NEWPRODUCT:
+      return generateNewProduct(subject, language)
+    case SUSTAINABILITY:
+      return generateSustainability(date, subject, language)
+    default:
+      throw new Error('Switch case error')
+  }
+}
+
+function generateEvent(date, subject, language) {
+  return `${COMMON_PROMPT} Sobre un evento que se celebrará en nuestra tienda el próximo ${date}, invitándole a inscribirse y a asistir. El tema principal del evento es ${subject}. ${
+    language === 'es-ca' ? CATALAN_PROMPT : ''
+  }`
+}
+function generateSustainability(date, subject, language) {
+  return `${COMMON_PROMPT} Sobre un evento de sostenibilidad que se celebrará el próximo ${date}, invitándole a inscribirse y a asistir. El tema principal del evento es ${subject}. Ten en cuenta el carácter verde y respetuoso del evento. ${
+    language === 'es-ca' ? CATALAN_PROMPT : ''
+  }`
+}
+
+function generateNewProduct(subject, language) {
+  return `${COMMON_PROMPT} Sobre un nuevo producto, presentación en nuestra tienda, invitándole a venir a descubrirlo. El tema principal del mensaje es ${subject}. ${
+    language === 'es-ca' ? CATALAN_PROMPT : ''
+  }`
+}
+
+function generateMaintenance(subject, language) {
+  return `Elabora un mensaje para un cliente, informal, en primera persona del plural, sobre un un servicio postventa asociado a su producto. La idea a comunicar sobre el mantenimiento es ${subject}. Responde en dos partes: primero el asunto terminado con un emoji, y después el cuerpo del mensaje. Sin el saludo ni la despedida, ni la firma. Devuelve la respuesta en formato json, una clave para el asunto y otra para el mensaje. ${
+    language === 'es-ca' ? CATALAN_PROMPT : ''
+  }`
 }
 
 function generateContent(animal) {
--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,49 @@ import { useState } from 'react'
 import styles from './index.module.css'
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState('')
+  const [subject, setSubject] = useState('')
   const [result, setResult] = useState()
+  const [asunto, setAsunto] = useState()
+  const [mensaje, setMensaje] = useState()
+  const [eventDate, setEventDate] = useState('')
+  const [type, setType] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [language, setLanguage] = useState('es-es')
 
   async function onSubmit(event) {
     event.preventDefault()
     try {
+      setLoading(true)
       const response = await fetch('/api/generate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ animal: animalInput })
+        body: JSON.stringify({
+          type: type,
+          subject: subject,
+          date: eventDate,
+          language: language
+        })
       })
 
       const data = await response.json()
       if (response.status !== 200) {
+        setLoading(false)
         throw (
           data.error ||
           new Error(`Request failed with status ${response.status}`)
         )
       }
-
+      setLoading(false)
+      setAsunto(JSON.parse(data.result).asunto)
+      setMensaje(JSON.parse(data.result).mensaje)
       setResult(data.result)
-      setAnimalInput('')
+      alert(
+        'Recuerda revisar siempre el contenido y reescribirlo en tus propias palabras.'
+      )
+      setSubject('')
+      setEventDate('')
     } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error)
@@ -38,29 +57,133 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>MailCraft</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3 className="vtmn-typo_title-3">Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            className="vtmn-text-input"
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input
-            type="submit"
-            className="vtmn-btn vtmn-btn_size--stretched"
-            value="Generate names"
-          />
-        </form>
-        <div className={styles.result}>{result}</div>
+        <img src="/logo.png" className={styles.icon} />
+        <h3 className="vtmn-typo_title-3">MailCraft</h3>
+        <div className={styles.boxes}>
+          <div>
+            <form onSubmit={onSubmit}>
+              <label>
+                <input
+                  type="radio"
+                  value="es-es"
+                  name="language"
+                  onChange={(e) => setLanguage(e.target.value)}
+                  defaultChecked={true}
+                />
+                Castellano
+              </label>
+              <label htmlFor="catalan">
+                <input
+                  type="radio"
+                  value="es-ca"
+                  name="language"
+                  onChange={(e) => setLanguage(e.target.value)}
+                />
+                Catalán
+              </label>
+
+              <div className="vtmn-select_container">
+                <label htmlFor="type">Tipo</label>
+
+                <select
+                  name="type"
+                  id="type"
+                  autoComplete="on"
+                  onChange={(e) => setType(e.target.value)}
+                  defaultValue="select"
+                >
+                  <option value="select" disabled>
+                    Select
+                  </option>
+                  <option value="event">Evento</option>
+                  <option value="maintenance">Servicio mantenimiento</option>
+                  <option value="clearance" disabled>
+                    Últimas unidades
+                  </option>
+                  <option value="sustainability-event">
+                    Acción sostenibilidad
+                  </option>
+                  <option value="new-product">Nuevo producto</option>
+                </select>
+              </div>
+
+              <div>
+                <label className="vtmn-text-input_label" htmlFor="date">
+                  Fecha
+                </label>
+                <div className="vtmn-text-input_container">
+                  <input
+                    type="date"
+                    className="vtmn-text-input"
+                    id="date"
+                    name="date"
+                    value={eventDate}
+                    onChange={(e) => setEventDate(e.target.value)}
+                    disabled={!['event', 'sustainability-event'].includes(type)}
+                  />
+                </div>
+              </div>
+
+              <label htmlFor="subject">Tema</label>
+
+              <input
+                id="subject"
+                className="vtmn-text-input"
+                type="text"
+                name="subject"
+                placeholder="Idea principal a comunicar"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+              <input
+                type="submit"
+                className="vtmn-btn vtmn-btn_size--stretched"
+                value="Generar"
+                disabled={loading}
+              />
+            </form>
+          </div>
+          <div>
+            {result && (
+              <div
+                className={`vtmn-card vtmn-card_variant--top-image ${styles.cardExample}`}
+              >
+                <div class="vtmn-card_image">
+                  <img
+                    src="https://storage.googleapis.com/dkt-design-cdn/images/landscape-placeholder.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="vtmn-card_content">
+                  <h2 className="vtmn-card_content--title">Asunto: {asunto}</h2>
+                  <span className="vtmn-card_content--body">{mensaje}</span>
+                </div>
+              </div>
+            )}
+            {loading && (
+              <div className={styles.cardExample}>
+                <span
+                  className={'vtmn-skeleton ' + styles.cardExample_figure}
+                ></span>
+                <div className={styles.cardExample_body}>
+                  <span
+                    className="vtmn-skeleton vtmn-skeleton_line"
+                    style={{ width: '95%' }}
+                  ></span>
+                  <span
+                    className="vtmn-skeleton vtmn-skeleton_line"
+                    style={{ width: '70%' }}
+                  ></span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </main>
     </div>
   )
